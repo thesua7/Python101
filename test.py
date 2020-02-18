849102e1b4d7dbdb319f580693c63d0e9abd5c26
Sani Ahamed <thesua711@gmail.com>
2020-02-19 01:58:00 +0600

    bhungbhang

---
+++ test.py
@@ -0,0 +1,17 @@
+
+num = input("Enter your first number: ")
+num1 = input("Enter your second number: ")
+
+if(num>num1):
+    print(num+" is greater")
+elif (num1>num):
+    print(num1+" is greater")
+else:
+    print("Equal")
+
+
+
+
+
+# for x in range(5):
+#     print(x)
